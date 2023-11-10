e61cd8d88da7ad1f13881b2460f5afe452e704aa
ryan-cho-i <chlqudtn347@gmail.com>
2023-11-10 12:10:42 -0500

    Initial Commit

---
+++ ThreadsTutorial/ThreadsTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsTutorialApp.swift
+//  ThreadsTutorial
+//
+//  Created by soo on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
