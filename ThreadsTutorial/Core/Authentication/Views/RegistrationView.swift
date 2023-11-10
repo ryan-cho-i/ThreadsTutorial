bf4538f8de84a320fdeb3c2e6542981fbd2c2aec
ryan-cho-i <chlqudtn347@gmail.com>
2023-11-10 12:44:18 -0500

    Login & Sign Up UI

--- ThreadsTutorial/Core/Authentication/Views/RegistrationView.swift
+++ ThreadsTutorial/Core/Authentication/Views/RegistrationView.swift
@@ -8,8 +8,68 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    @State private var fullname = ""
+    @State private var username = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            
+            Image ("threads-app-icon")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 120, height: 120)
+                .padding()
+            
+            VStack {
+                TextField("Enter your email", text: $email)
+                    .modifier(ThreadsTextFieldModifier())
+                
+                SecureField("Enter your password", text: $password)
+                    .modifier(ThreadsTextFieldModifier())
+                
+                TextField("Enter your fullname", text: $fullname)
+                    .modifier(ThreadsTextFieldModifier())
+                TextField("Enter your username", text: $username    )
+                    .modifier(ThreadsTextFieldModifier())
+            }
+            
+            Button {
+                
+            } label: {
+                Text("Sign Up")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .frame(width: 352, height: 44)
+                    .background(.black)
+                    .cornerRadius(8)
+            }
+            .padding(.vertical)
+            
+            Spacer()
+            
+            Divider()
+            
+            Button {
+                
+            } label: {
+                
+                HStack (spacing: 3) {
+                    Text ("Already have an account?")
+                    
+                    Text ("Sign In")
+                        .fontWeight(.semibold)
+                }
+                .foregroundColor(.black)
+                .font(.footnote)
+            }
+            .padding(.vertical, 16)
+            
+        }
     }
 }
 
