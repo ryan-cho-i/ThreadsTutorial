7280728bcd7d2616792b5ed52b91f887f67b934a
ryan-cho-i <chlqudtn347@gmail.com>
2023-11-17 17:50:51 -0500

    Thread Replies in Profile

---
+++ ThreadsTutorial/Core/Profile/Components/View/ThreadReplyProfileCell.swift
@@ -0,0 +1,18 @@
+//
+//  ThreadReplyProfileCell.swift
+//  ThreadsTutorial
+//
+//  Created by soo on 11/17/23.
+//
+
+import SwiftUI
+
+struct ThreadReplyProfileCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThreadReplyProfileCell()
+}
